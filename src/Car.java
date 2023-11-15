6d0091d5645563ac4e7906fc139bc4ffbee6d03f
Loura <Loura@chalmers.se>
2023-11-15 08:48:44 +0100

    Changes by johui

--- src/Car.java
+++ src/Car.java
@@ -55,7 +55,7 @@ abstract class Car implements Movable  {
     }
 
 
-    protected void gas(double amount) {
+    public void gas(double amount) {
         if (amount>= 0 && amount <= 1)
         {
             incrementSpeed(amount);
